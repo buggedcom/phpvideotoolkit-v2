c9086f20ae8bf97aa84ab7f21b06f677d67faa75
Oliver Lillie <ollie@buggedcom.co.uk>
2013-02-28 01:14:04 +0200

    added temporary class for outputing images

---
+++ lib/Image.php
@@ -0,0 +1,81 @@
+<?php
+	
+	/**
+	 * This file is part of the PHP Video Toolkit v2 package.
+	 *
+	 * @author Oliver Lillie (aka buggedcom) <publicmail@buggedcom.co.uk>
+	 * @license Dual licensed under MIT and GPLv2
+	 * @copyright Copyright (c) 2008 Oliver Lillie <http://www.buggedcom.co.uk>
+	 * @package PHPVideoToolkit V2
+	 * @version 2.0.0.a
+	 * @uses ffmpeg http://ffmpeg.sourceforge.net/
+	 */
+	 
+	namespace PHPVideoToolkit;
+
+	/**
+	 * This class provides generic data parsing for the output from FFmpeg from specific
+	 * media files. Parts of the code borrow heavily from Jorrit Schippers version of 
+	 * PHPVideoToolkit v 0.1.9.
+	 *
+	 * @access public
+	 * @author Oliver Lillie
+	 * @author Jorrit Schippers
+	 * @package default
+	 */
+	class Image extends Media
+	{
+		public function __construct($image_file_path, ImageFormat $image_input_format=null, $ffmpeg_path, $temp_directory)
+		{
+			parent::__construct($image_file_path, $image_input_format, $ffmpeg_path, $temp_directory);
+			
+//			validate this media file is a image file
+			$type = $this->readType();
+			if($type !== 'image')
+			{
+				throw new Exception('You cannot use an instance of '.get_class($this).' for "'.$image_file_path.'" as the file is not a image file. It is reported to be a '.$type);
+			}
+		}
+		
+		/**
+		 * Returns the default (empty) input format for the type of media object this class is.
+		 *
+		 * @access public
+		 * @author Oliver Lillie
+		 * @param string $type Either input for an input format or output for an output format.
+		 * @return Format
+		 */
+		public function getDefaultFormat($type)
+		{
+			return $this->_getDefaultFormat($type, 'VideoFormat');
+		}
+		
+		/**
+		 * Returns any video information about the file if available.
+		 *
+		 * @access public
+		 * @author Oliver Lillie
+		 * @param boolean $read_from_cache 
+		 * @return mixed Returns an array of found data, otherwise returns null.
+		 */
+		public function readDimensions($read_from_cache=true)
+		{
+			$video_data = parent::readVideoComponent($read_from_cache);
+			return $video_data['dimensions'];
+		}
+		
+		/**
+		 * Returns any video information about the file if available.
+		 *
+		 * @access public
+		 * @author Oliver Lillie
+		 * @param boolean $read_from_cache 
+		 * @return mixed Returns an array of found data, otherwise returns null.
+		 */
+		public function readFrameRate($read_from_cache=true)
+		{
+			$video_data = parent::readVideoComponent($read_from_cache);
+			return $video_data['frame_rate'];
+		}
+	}
+
