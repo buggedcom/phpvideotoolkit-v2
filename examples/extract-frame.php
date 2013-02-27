605a04b7f65a0a4d5f844fe01aca4c37ea5f5f84
Oliver Lillie <ollie@buggedcom.co.uk>
2013-02-28 01:13:52 +0200

    added two very basic examples

---
+++ examples/extract-frame.php
@@ -0,0 +1,50 @@
+<?php
+
+	ini_set('error_reporting', '1');
+	ini_set('track_errors', '1');
+	ini_set('display_errors', '1');
+	ini_set('display_startup_errors', '1');
+	
+//  define the error callback
+    function __errorHandler()
+    {      
+        $args = func_get_args();      
+        $count = func_num_args();  
+		\PHPVideoToolkit\Trace::vars('ERROR---------', $count === 1 ? 'exception' : 'error', $args);
+    }
+    set_error_handler('__errorHandler');
+    set_exception_handler('__errorHandler');
+	
+	
+	require_once '../vendor/autoload.php';
+	require_once '../autoloader.php';
+	
+//	require '../vendor/autoload.php';
+	// $stash = new Stash\Pool();
+	// print_r($stash);
+	// exit;
+	
+	try
+	{
+		\PHPVideoToolkit\Factory::setDefaultVars('./tmp', '/opt/local/bin');
+
+		$output_format = \PHPVideoToolkit\Factory::videoFormat('output');
+		//$output_format->setVideoFormat('ljpeg');
+		
+ 		$video = \PHPVideoToolkit\Factory::video('media/BigBuckBunny_320x180.mp4');
+		
+		$video->extractFrame(new \PHPVideoToolkit\Timecode(50));
+		
+		$result = $video
+			->save('./output/test-'.time().'.jpg', $output_format, \PHPVideoToolkit\Video::OVERWRITE_EXISTING, $progress_handler);
+		
+		\PHPVideoToolkit\Trace::vars($result);
+	}
+	catch(\PHPVideoToolkit\Exception $e)
+	{
+		
+		\PHPVideoToolkit\Trace::vars($video->getProcess()->getExecutedCommand());
+		\PHPVideoToolkit\Trace::vars($video->getProcess()->getBuffer());
+		\PHPVideoToolkit\Trace::vars($video->getProcess()->getLastSplit());
+		\PHPVideoToolkit\Trace::vars($e);
+	}
