4580505b86a7547512ab6bbaa62abeb28648f154
Oliver Lillie <ollie@buggedcom.co.uk>
2013-03-04 01:49:15 +0200

    udated flv example

--- examples/convert-to-flv.php
+++ examples/convert-to-flv.php
@@ -29,35 +29,38 @@
 		\PHPVideoToolkit\Factory::setDefaultVars('./tmp', '/opt/local/bin');
 
  		$video = \PHPVideoToolkit\Factory::video('media/BigBuckBunny_320x180.mp4');
+		$process = $video->getProcess();
+		$process->setProcessTimelimit(1);
 		$output = $video->extractSegment(new \PHPVideoToolkit\Timecode(10), new \PHPVideoToolkit\Timecode(20))
 						->save('./output/big_buck_bunny.flv');
 		
-		$process = $video->getProcess();
 		
 		echo '<h1>Executed Command</h1>';
 		\PHPVideoToolkit\Trace::vars($process->getExecutedCommand());
 		echo '<hr /><h1>FFmpeg Process Messages</h1>';
 		\PHPVideoToolkit\Trace::vars($process->getMessages());
 		echo '<hr /><h1>Buffer Output</h1>';
-		\PHPVideoToolkit\Trace::vars($process->getBuffer());
+		\PHPVideoToolkit\Trace::vars($process->getBuffer(true));
 		
 	}
 	catch(\PHPVideoToolkit\FfmpegProcessOutputException $e)
 	{
+		echo '<h1>Error</h1>';
+		\PHPVideoToolkit\Trace::vars($e);
+
 		$process = $video->getProcess();
-		
 		if($process->isCompleted())
 		{
-			echo '<h1>Executed Command</h1>';
+			echo '<hr /><h2>Executed Command</h2>';
 			\PHPVideoToolkit\Trace::vars($process->getExecutedCommand());
-			echo '<hr /><h1>FFmpeg Process Messages</h1>';
+			echo '<hr /><h2>FFmpeg Process Messages</h2>';
 			\PHPVideoToolkit\Trace::vars($process->getMessages());
-			echo '<hr /><h1>Buffer Output</h1>';
-			\PHPVideoToolkit\Trace::vars($process->getBuffer());
+			echo '<hr /><h2>Buffer Output</h2>';
+			\PHPVideoToolkit\Trace::vars($process->getBuffer(true));
 		}
-		\PHPVideoToolkit\Trace::vars($e);
 	}
 	catch(\PHPVideoToolkit\Exception $e)
 	{
+		echo '<h1>Error</h1>';
 		\PHPVideoToolkit\Trace::vars($e);
 	}
