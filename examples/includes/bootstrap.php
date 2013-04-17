dfb323672297ef283db4d8754632b893bb5ac0c7
Oliver Lillie <ollie@buggedcom.co.uk>
2013-04-17 18:15:04 +0300

    fixed bug in example bootstrap process

--- examples/includes/bootstrap.php
+++ examples/includes/bootstrap.php
@@ -9,14 +9,15 @@
     function __errorHandler()
     {      
         $args = func_get_args();      
-        $count = func_num_args();  
+        $count = func_num_args(); 
 		\PHPVideoToolkit\Trace::vars('ERROR---------', $count === 1 ? 'exception' : 'error', $args);
     }
     set_error_handler('__errorHandler');
     set_exception_handler('__errorHandler');
 	
 	$basedir = dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR;
-
-	require_once $basedir.'vendor/autoload.php';
-	require_once $basedir.'autoloader.php';
-	require_once $basedir.'examples/includes/config.php';
+    define('BASE', $basedir);
+    
+	require_once BASE.'vendor/autoload.php';
+	require_once BASE.'autoloader.php';
+	require_once BASE.'examples/includes/config.php';
