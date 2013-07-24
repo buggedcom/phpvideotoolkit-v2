abd804d4f3f0c06537ef3e8d29d63d62a0ed1af4
Oliver Lillie <ollie@buggedcom.co.uk>
2013-07-24 17:33:03 +0300

    updated example to better trace exceptions

--- examples/extract-frames.php
+++ examples/extract-frames.php
@@ -2,20 +2,31 @@
 
 	include_once './includes/bootstrap.php';
 	
-	try
-	{
- 		$video = new \PHPVideoToolkit\Video($example_video_path, $config);
-		$output = $video->extractFrames(new \PHPVideoToolkit\Timecode(40), new \PHPVideoToolkit\Timecode(50))
-			   			->save('./output/big_buck_bunny_frame_%timecode.jpg');
+    try
+    {
+         $video = new \PHPVideoToolkit\Video($example_video_path, $config);
+    }
+    catch(\PHPVideoToolkit\Exception $e)
+    {
+        \PHPVideoToolkit\Trace::vars($e);
+    }
+    
+    if($video)
+    {
+        try
+        {
+    		$output = $video->extractFrames(new \PHPVideoToolkit\Timecode(40), new \PHPVideoToolkit\Timecode(50))
+    			   			->save('./output/big_buck_bunny_frame_%timecode.jpg');
 		
-		// $output is an array of \PHPVideoToolkit\Image objects upon success.
-	}
-	catch(\PHPVideoToolkit\Exception $e)
-	{
-		if($video->getProcess()->isCompleted())
-		{
-			\PHPVideoToolkit\Trace::vars($video->getProcess()->getExecutedCommand());
-			\PHPVideoToolkit\Trace::vars($video->getProcess()->getBuffer());
-		}
-		\PHPVideoToolkit\Trace::vars($e);
-	}
+    		// $output is an array of \PHPVideoToolkit\Image objects upon success.
+        }
+        catch(\PHPVideoToolkit\Exception $e)
+        {
+            if($video->getProcess()->isCompleted())
+            {
+                \PHPVideoToolkit\Trace::vars($video->getProcess()->getExecutedCommand());
+                \PHPVideoToolkit\Trace::vars($video->getProcess()->getBuffer());
+            }
+            \PHPVideoToolkit\Trace::vars($e);
+        }
+    }
