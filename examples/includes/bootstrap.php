612975931059b91d58a89af807341bc52ea4096f
Oliver Lillie <buggedcom@gmail.com>
2014-06-09 21:40:36 +0300

    disabled error reporting in cli and added debug backtrace to errors

--- examples/includes/bootstrap.php
+++ examples/includes/bootstrap.php
@@ -1,16 +1,27 @@
 <?php
 
-    ini_set('error_reporting', '1');
-    ini_set('track_errors', '1');
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    
+    if($argv)
+    {
+        error_reporting(E_NONE);
+    }
+    else
+    {
+        ini_set('error_reporting', '1');
+        ini_set('track_errors', '1');
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+    }
+
 //  define the error callback
     function __errorHandler()
     {      
+        if(error_reporting() === 0)
+        {
+            return;
+        }
         $args = func_get_args();      
         $count = func_num_args(); 
-        \PHPVideoToolkit\Trace::vars('ERROR---------', $count === 1 ? 'exception' : 'error', $args);
+        \PHPVideoToolkit\Trace::vars('ERROR---------', $count === 1 ? 'exception' : 'error', $args, debug_backtrace());
     }
     set_error_handler('__errorHandler');
     set_exception_handler('__errorHandler');
