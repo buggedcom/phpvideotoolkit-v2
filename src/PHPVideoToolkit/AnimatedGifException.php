2ec2bc034df62f99ef36722a7a4d9e1794e2b15a
Oliver Lillie <buggedcom@gmail.com>
2014-06-12 01:08:11 +0300

    added new exception that is thrown if an animated gif is not successfully created

---
+++ src/PHPVideoToolkit/AnimatedGifException.php
@@ -0,0 +1,18 @@
+<?php
+    
+    /**
+     * This file is part of the PHP Video Toolkit v2 package.
+     *
+     * @author Oliver Lillie (aka buggedcom) <publicmail@buggedcom.co.uk>
+     * @license Dual licensed under MIT and GPLv2
+     * @copyright Copyright (c) 2008-2014 Oliver Lillie <http://www.buggedcom.co.uk>
+     * @package PHPVideoToolkit V2
+     * @version 2.1.7-beta
+     * @uses ffmpeg http://ffmpeg.sourceforge.net/
+     */
+     
+    namespace PHPVideoToolkit;
+     
+    class AnimatedGifException extends \RuntimeException
+    {
+    }
